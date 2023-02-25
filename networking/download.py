b03fc0619720a891fef4638a7618842ec1385d45
Advik <advik.b@gmail.com>
2023-02-25 10:56:28 +0530

    Update download.py

--- networking/download.py
+++ networking/download.py
@@ -1,55 +1,78 @@
 from PyQt6.QtWidgets import QProgressBar, QPushButton, QVBoxLayout, QWidget
 from PyQt6.QtCore import QThread, pyqtSignal
-from PyQt6.QtWidgets import QApplication
 from pytube import YouTube
 from requests import get
-from pytube import Playlist
-from dataclasses import dataclass
+
 
 class DownloadThread(QThread):
     progress = pyqtSignal(int)
 
-    def __init__(self, url: str, parent=None):
+    def __init__(
+            self,
+            url: str,
+            parent=None,
+            save_path: str = None,
+            condition: str = "get_highest_resolution",
+            chunk_size: int = 1024,
+    ):
         super().__init__(parent)
         self.url = url
-
+        self.save_path = save_path
+        self.condition = condition
+        self.chunk_size = chunk_size
 
     def run(self):
-        print("Downloading", self.url)
         yt = YouTube(self.url)
-        stream = yt.streams.get_highest_resolution()
+
+        stream = getattr(yt.streams, self.condition)()  # Call the method with the name self.condition
         total_size = stream.filesize
         downloaded = 0
         video = stream.url
         r = get(video, stream=True)
-        with open("video.mp4", "wb") as f:
-            for chunk in r.iter_content(chunk_size=1024 * 1024):
+        with open(self.save_path, "wb") as f:
+            for chunk in r.iter_content(chunk_size=self.chunk_size):
                 downloaded += len(chunk)
                 f.write(chunk)
                 self.progress.emit(int(downloaded / total_size * 100))
-        print("Downloaded", self.url)
 
 
 class DownloadWidget(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, url: str = None, save_path: str = None, condition: str = "get_highest_resolution", chunk_size: int = 1024):
         super().__init__(parent)
         self.progressbar = QProgressBar()
         self.button = QPushButton("Download")
         self.button.clicked.connect(self.download)
+        self.url = url
+        self.save_path = save_path
+        self.condition = condition
+        self.chunk_size = chunk_size
+
         layout = QVBoxLayout()
         layout.addWidget(self.progressbar)
         layout.addWidget(self.button)
+        self.downloading = False
         self.setLayout(layout)
 
     def download(self):
-        self.thread = DownloadThread("https://www.youtube.com/watch?v=9bZkp7q19f0")
+        if self.downloading:
+            return
+
+        self.thread = DownloadThread(
+            self.url,
+            self,
+            self.save_path,
+            self.condition,
+            self.chunk_size,
+        )
         self.thread.progress.connect(self.progressbar.setValue)
         self.thread.start()
+        self.downloading = True
+        self.button.setText("Downloading...")
+        self.button.setEnabled(False)
+        self.thread.finished.connect(self.finished)
 
+    def finished(self):
+        self.downloading = False
+        self.button.setText("Download")
+        self.button.setEnabled(True)
 
-if __name__ == "__main__":
-    import sys
-    app = QApplication(sys.argv)
-    widget = DownloadWidget()
-    widget.show()
-    sys.exit(app.exec())
